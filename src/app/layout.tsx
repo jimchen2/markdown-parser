ca6f71607a1a66e2193c61f6090512ece7301835
Jim Chen <jimchen4214@gmail.com>
2024-09-21 02:23:47 +0800

    Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
-import '../styles/global.css'
+import '../styles/global.css';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
