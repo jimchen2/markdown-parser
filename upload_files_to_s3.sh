187f31e5601c4b4306ee4893810cfddef4a2a6da
Jim Chen <123833550+jimchen2@users.noreply.github.com>
2024-09-20 19:56:08 +0800

    Update upload_files_to_s3.sh

--- upload_files_to_s3.sh
+++ upload_files_to_s3.sh
@@ -7,7 +7,7 @@ for file in *; do
         uuid=$(uuidgen)
         extension="${file##*.}"
         new_name="${uuid}.${extension}"
-        rclone copy "$file" "${RCLONE_REMOTE}:${BUCKET_NAME}/${new_name}" -P
-        echo "${STATIC_HOST}/${new_name}"
+        rclone copyto "$file" "${RCLONE_REMOTE}:${BUCKET_NAME}/${new_name}" -P
+        echo "Original file: $file -> Uploaded as: ${STATIC_HOST}/${new_name}"
     fi
 done
